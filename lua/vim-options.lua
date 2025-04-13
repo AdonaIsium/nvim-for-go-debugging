45078192e7c15be47d0f883daf3cccf1a51ad57a
Lanre <lanre@Haliax0.local>
2025-04-13 10:51:51 -0500

    added line numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,9 @@
--- Set editor options
+-- Tab Behavior
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+
+-- Line Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
