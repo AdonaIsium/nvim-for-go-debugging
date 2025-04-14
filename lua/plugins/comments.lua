33ccb5f644f5f1e60d91675469399666137cac00
Lanre <lanre@Haliax0.local>
2025-04-14 11:51:30 -0500

    updated keybinding helpers a bit more

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,11 +1,11 @@
 return {
-  "numToStr/Comment.nvim",
-  event = "VeryLazy",
-  config = function()
-    require("Comment").setup()
+	"numToStr/Comment.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("Comment").setup()
 
-    vim.keymap.set("n", "<C-_>", function()
-      require("Comment.api").toggle.linewise.current()
-    end, { noremap = true, silent = true, desc = "Toggle comment (normal mode)" })
-  end,
+		vim.keymap.set("n", "<C-_>", function()
+			require("Comment.api").toggle.linewise.current()
+		end, { noremap = true, silent = true, desc = "Toggle comment (normal mode)" })
+	end,
 }
