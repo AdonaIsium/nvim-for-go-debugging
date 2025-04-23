444359bcf93431dbdf32adea67c905664af83d74
Lanre <lanre@Haliax0.local>
2025-04-23 10:35:24 -0500

    matty is a hero!

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,10 +1,47 @@
 -- Tab Behavior
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
 
 -- Line Numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Case
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Search
+vim.opt.hlsearch = false
+
+-- Scrolling
+vim.opt.sidescrolloff = 8
+vim.opt.scrolloff = 8
+
+-- Responsiveness
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Split Behavior
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- White Space
+vim.opt.list = true
+vim.opt.listchars = { tab = " ", trail = "" }
+
+-- Incremental Editing Preview
+vim.opt.inccommand = "split"
+
+-- Highlight Line
+vim.opt.cursorline = true
+
+-- Color Column
+vim.opt.colorcolumn = "80"
+
+-- Color Column for git commits
+vim.cmd("autocmd FileType gitcommit set colorcolumn=50,72")
+vim.cmd("autocmd BufRead,BufNewFile COMMIT set colorcolumn=50,72")
+vim.cmd("autocmd BufRead,BufNewFile COMMIT set textwidth=72")
