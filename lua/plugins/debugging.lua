fb65e67a9fa274463d05969da6317385e263f65e
Lanre <lanre@Haliax0.local>
2025-04-28 15:03:53 -0500

    changed debugging.lua to allow for tray to be opened by itself

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -6,34 +6,38 @@ return {
 			local dap = require("dap")
 			local dapui = require("dapui")
 
-			require("dapui").setup()
-			require("dap-go").setup()
-			-- If we want ui to open when we start a debugging session, uncomment these.
-			-- dap.listeners.before.attach.dapui_config = function()
-			-- 	dapui.open()
-			-- end
-			-- dap.listeners.before.launch.dapui_config = function()
-			-- 	dapui.open()
-			-- end
-			-- dap.listeners.before.event_terminated.dapui_config = function()
-			-- 	dapui.open()
-			-- end
-			-- dap.listeners.before.event_exited.dapui_config = function()
-			-- 	dapui.open()
-			-- end
+			dapui.setup({
+				layouts = {
+					{
+						elements = {
+							{ id = "watches", size = 0.15 },
+							{ id = "scopes", size = 0.40 },
+							{ id = "breakpoints", size = 0.20 },
+							{ id = "stacks", size = 0.20 },
+						},
+						size = 40,
+						position = "left",
+					},
+					{
+						elements = {
+							{ id = "repl", size = 1 },
+						},
+						size = 11,
+						position = "bottom",
+					},
+				},
+			})
 
-			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+			require("dap-go").setup()
+
+			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint)
 			vim.keymap.set("n", "<Leader>dT", function()
 				dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 			end)
-			vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-			vim.keymap.set("n", "F3", dap.continue, {})
+			vim.keymap.set("n", "<Leader>dC", dap.continue)
 			vim.keymap.set("n", "<Leader>do", dap.step_over)
-			vim.keymap.set("n", "F4", dap.step_over)
 			vim.keymap.set("n", "<Leader>di", dap.step_into)
-			vim.keymap.set("n", "F5", dap.step_into)
 			vim.keymap.set("n", "<Leader>dO", dap.step_out)
-			vim.keymap.set("n", "F6", dap.step_out)
 
 			vim.keymap.set("n", "<Leader>dgt", function()
 				require("dap-go").debug_test()
@@ -48,26 +52,31 @@ return {
 			end)
 			vim.keymap.set("n", "<Leader>dr", dap.run_last)
 			vim.keymap.set("n", "<Leader>dq", function()
-				require("dap").terminate()
-				require("dapui").close()
-			end, { desc = "DAP: Terminate session & Close UI" })
+				dap.terminate()
+				dapui.close()
+			end, { desc = "DAP: Terminate session & close UI" })
 
-			-- DAP UI
+			vim.keymap.set("n", "<Leader>du", function()
+				dapui.toggle({})
+			end, { desc = "DAP: Toggle Full UI (Scopes + REPL + Controls)" })
+
+			vim.keymap.set("n", "<Leader>dc", function()
+				dapui.toggle({ layout = 2, reset = true })
+			end, { desc = "DAP: Open only REPL + Controls (Minimal)" })
+
 			local sidebar = nil
-
-			vim.keymap.set("n", "<leader>dus", function()
+			vim.keymap.set("n", "<Leader>ds", function()
 				local widgets = require("dap.ui.widgets")
 				if not sidebar then
 					sidebar = widgets.sidebar(widgets.scopes)
 				end
-				-- Toggle based on whether it's currently open
 				if sidebar._opened then
 					sidebar.close()
 				else
 					sidebar.open()
 				end
-			end, { desc = "DAP: Toggle scopes sidebar" })
-			vim.keymap.set("n", "<Leader>du", dapui.toggle)
+			end, { desc = "DAP: Toggle Scopes Sidebar" })
+
 			vim.keymap.set("n", "<Leader>de", dapui.eval)
 		end,
 	},
