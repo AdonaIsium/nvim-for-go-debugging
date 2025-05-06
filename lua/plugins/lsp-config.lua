bb35b5cdfae4c69af7f99d5d1ee989c88374bea8
Lanre <lanre@Haliax0.local>
2025-05-06 18:21:27 -0500

    fixed mason settings which were broken by recent update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,14 +17,13 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
+			local mason_lspconfig = require("mason-lspconfig")
 
 			local on_attach = function(client, bufnr)
-				-- Auto format + organize imports for Go
 				if vim.bo[bufnr].filetype == "go" then
 					vim.api.nvim_create_autocmd("BufWritePre", {
 						buffer = bufnr,
 						callback = function()
-							-- Organize imports via gopls code actions
 							local params = vim.lsp.util.make_range_params()
 							params.context = { only = { "source.organizeImports" } }
 							local result = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params, 1000)
@@ -37,13 +36,10 @@ return {
 									end
 								end
 							end
-
-							-- Format the Go file
 							vim.lsp.buf.format({ async = false })
 						end,
 					})
 				elseif client.name == "null-ls" then
-					-- Format other files on save using null-ls
 					vim.api.nvim_create_autocmd("BufWritePre", {
 						buffer = bufnr,
 						callback = function()
@@ -57,7 +53,6 @@ return {
 					})
 				end
 
-				-- Keybindings
 				local nmap = function(keys, func, desc)
 					if desc then
 						desc = "LSP: " .. desc
@@ -101,36 +96,31 @@ return {
 				end, { buffer = bufnr, desc = "Format File (stylua)" })
 			end
 
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					local opts = {
-						on_attach = on_attach,
-						capabilities = capabilities,
-					}
+			for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
+				local opts = {
+					on_attach = on_attach,
+					capabilities = capabilities,
+				}
 
-					-- gopls specific config
-					if server_name == "gopls" then
-						opts.settings = {
-							gopls = {
-								analyses = {
-									unusedparams = true,
-									unusedwrite = true,
-								},
-								staticcheck = true,
+				if server_name == "gopls" then
+					opts.settings = {
+						gopls = {
+							analyses = {
+								unusedparams = true,
+								unusedwrite = true,
 							},
-						}
-						opts.on_init = function(client)
-							client.server_capabilities.documentFormattingProvider = true
-						end
+							staticcheck = true,
+						},
+					}
+					opts.on_init = function(client)
+						client.server_capabilities.documentFormattingProvider = true
 					end
+				end
 
-					lspconfig[server_name].setup(opts)
-				end,
-			})
+				lspconfig[server_name].setup(opts)
+			end
 		end,
 	},
-
-	-- null-ls setup for formatting
 	{
 		"nvimtools/none-ls.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
