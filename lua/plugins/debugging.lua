22b58ae828be745dfd22b42719f5e7596f2e44a6
Lanre <lanre.arisen@gmail.com>
2025-09-15 13:18:05 -0500

    woopsie had same plugin loaded twice

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,11 +1,16 @@
 return {
 	{
 		"mfussenegger/nvim-dap",
-		dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio", "leoluz/nvim-dap-go" },
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+			"leoluz/nvim-dap-go",
+		},
 		config = function()
 			local dap = require("dap")
 			local dapui = require("dapui")
 
+			-- UI setup
 			dapui.setup({
 				layouts = {
 					{
@@ -28,8 +33,41 @@ return {
 				},
 			})
 
+			-- Go adapter
 			require("dap-go").setup()
 
+			-- Rust adapter (codelldb installed via Mason)
+			local codelldb_path = vim.fn.stdpath("data") .. "/mason/bin/codelldb"
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = codelldb_path,
+					args = { "--port", "${port}" },
+				},
+			}
+			dap.configurations.rust = {
+				{
+					name = "Launch Rust executable",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+				},
+				{
+					name = "Attach to process",
+					type = "codelldb",
+					request = "attach",
+					pid = require("dap.utils").pick_process,
+					cwd = "${workspaceFolder}",
+				},
+			}
+
+			-- Keymaps
 			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint)
 			vim.keymap.set("n", "<Leader>dT", function()
 				dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
@@ -39,14 +77,15 @@ return {
 			vim.keymap.set("n", "<Leader>di", dap.step_into)
 			vim.keymap.set("n", "<Leader>dO", dap.step_out)
 
+			-- Go test helpers
 			vim.keymap.set("n", "<Leader>dgt", function()
 				require("dap-go").debug_test()
 			end, { desc = "DAP Go: Debug test" })
-
 			vim.keymap.set("n", "<Leader>dgl", function()
 				require("dap-go").debug_last()
 			end, { desc = "DAP Go: Debug last test" })
 
+			-- Misc controls
 			vim.keymap.set("n", "<Leader>lp", function()
 				dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
 			end)
@@ -55,15 +94,14 @@ return {
 				dap.terminate()
 				dapui.close()
 			end, { desc = "DAP: Terminate session & close UI" })
-
 			vim.keymap.set("n", "<Leader>du", function()
 				dapui.toggle({})
 			end, { desc = "DAP: Toggle Full UI (Scopes + REPL + Controls)" })
-
 			vim.keymap.set("n", "<Leader>dc", function()
 				dapui.toggle({ layout = 2, reset = true })
 			end, { desc = "DAP: Open only REPL + Controls (Minimal)" })
 
+			-- Sidebar
 			local sidebar = nil
 			vim.keymap.set("n", "<Leader>ds", function()
 				local widgets = require("dap.ui.widgets")
